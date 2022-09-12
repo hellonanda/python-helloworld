8fd687b68f09be90caaf71af670dccbd89ea543d
Nandak99 <54415494+Nandak99@users.noreply.github.com>
2022-09-12 23:30:35 +0530

    Update __init__.py

--- helloworld/__init__.py
+++ helloworld/__init__.py
@@ -1,4 +1,4 @@
-def __read_version_txt():
 import pkgutil
+def __read_version_txt():
 return pkgutil.get_data('helloworld', 'VERSION.txt').decode('utf-8').strip()
 __version__ = __read_version_txt()
